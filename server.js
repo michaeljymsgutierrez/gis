303d88b58f871f44306952e22e0483939a5d3b48
michaeljymsgutierrez <leeez711@gmail.com>
2017-12-26 23:26:03 +0800

    Updated server to development mode

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'); // ExperssJS Framework
 var app = express(); // Invoke express to variable for use in application
-var port = process.env.PORT || 8080; // Set default port or assign a port in enviornment
+var port = process.env.PORT || 3000; // Set default port or assign a port in enviornment
 var morgan = require('morgan'); // Import Morgan Package
 var mongoose = require('mongoose'); // HTTP request logger middleware for Node.js
 var bodyParser = require('body-parser'); // Node.js body parsing middleware. Parses incoming request bodies in a middleware before your handlers, available under req.body.
