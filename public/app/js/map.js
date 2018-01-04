f9f4a8b6286d148f96ab5956a924b3a56f3c5349
Chaelgutierrez <michael@vielsoft.com>
2018-01-04 20:13:29 +0800

    Added disease plot on maps

--- public/app/js/map.js
+++ public/app/js/map.js
@@ -1,76 +1,64 @@
 function initMap() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            curLat = position.coords.latitude;
+            curLong = position.coords.longitude;
+        });
+    }
+
     function reInit() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 13.9419, lng: 121.1644 },
-            zoom: 11
-        });
-        var card = document.getElementById('pac-card');
-        var input = document.getElementById('pac-input');
-        var countries = document.getElementById('country-selector');
+        $.ajax({
+            url: "http://127.0.0.1:3000/api/disease/all",
+            method: "GET",
+            headers: {
+                "x-access-token": window.localStorage.getItem('token')
+            },
+            processData: false,
+            success: function(res) {
+                var data = res;
+                var locations = [];
+                var counter = 0;
 
-        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+                data.forEach(function(disease, key) {
+                    var toPush = [];
+                    toPush[0] = "<b>Poultry Name: </b> " + disease.poultryname + "<br/>" +
+                        "<b>Poultry Address: </b>" + disease.poultryaddress + "<br/>" +
+                        "<b>Disease: </b>" + disease.disease;
+                    toPush[1] = JSON.parse(disease.location[1]);
+                    toPush[2] = JSON.parse(disease.location[0]);
+                    toPush[3] = key + 1;
+                    locations.push(toPush);
+                });
 
-        var autocomplete = new google.maps.places.Autocomplete(input);
+                console.log(curLat + "-" + curLong);
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 10,
+                    center: new google.maps.LatLng(curLat, curLong),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
 
-        // Set initial restrict to the greater list of countries.
-        autocomplete.setComponentRestrictions({ 'country': ['ph'] });
+                var infowindow = new google.maps.InfoWindow();
 
-        var infowindow = new google.maps.InfoWindow();
-        var infowindowContent = document.getElementById('infowindow-content');
-        infowindow.setContent(infowindowContent);
+                var marker, i;
 
-        var image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGkSURBVDhPldRJKEVhGMbxY47EwlQUkSlFWbASCkVKWdhYiY1kTDayskBRspIobCTKQspUNqwslMSCZMpUCiVDxv9zukfn4rr3PvXr3O+M73e+91zDi/igAbt4wQn6EAyvMgDdYBbrmMY91uAPj5KHDwwjGiGIRC3eUAO3CcQ2NpGtHbakYAuq7t/ovYxAT22CH+xJhm40Y45cJAsreEc/0vAzubhBjzmyRRWUYhl6J1fQysQjCHGwonPb8Iki7bBSgh3owBFGkQ9fKFN4Rbo5MoxUzOMS31NuhyrYd/zWSXqiPc3YQIQ5MowqqB06zRHR0qqKSWjOnjRYIlTNNUK1Q1nAMXKgEruwhEZYCXBsFU21A1qEOu2wcopxqMnUYKrOUgY98REJULXl0HtZhNP0nzAEtXkv7DdqgZIBXVSIA5whFk7ZwxxioAvuoJtcwFpuVaslXoWOZ+JXuqGDFQhDFHRROBR9ItU4h84rwJ/RxSpXHdqKJCjaFmMCt1BrWD3kMlpOfZTqpUPo29H/zgOeMQirQrfRslZiDGoJrWQ9NFU3MYwvoLZeYPTQcOUAAAAASUVORK5CYII=';
-        var marker = new google.maps.Marker({
-            map: map,
-            anchorPoint: new google.maps.Point(0, -29),
-            icon: image
-        });
+                for (i = 0; i < locations.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
 
-        autocomplete.addListener('place_changed', function() {
-            infowindow.close();
-            marker.setVisible(false);
-            var place = autocomplete.getPlace();
-            if (!place.geometry) {
-                // User entered the name of a Place that was not suggested and
-                // pressed the Enter key, or the Place Details request failed.
-                window.alert("No details available for input: '" + place.name + "'");
-                return;
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        }
+                    })(marker, i));
+                }
+            },
+            error: function(err) {
+                console.log(err);
             }
-
-            // If the place has a geometry, then present it on a map.
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(13); // Why 17? Because it looks good.
-            }
-            marker.setPosition(place.geometry.location);
-            marker.setVisible(true);
-
-            var address = '';
-            if (place.address_components) {
-                address = [
-                    (place.address_components[0] && place.address_components[0].short_name || ''),
-                    (place.address_components[1] && place.address_components[1].short_name || ''),
-                    (place.address_components[2] && place.address_components[2].short_name || '')
-                ].join(' ');
-            }
-            infowindowContent.children['place-name'].textContent = place.name;
-            infowindowContent.children['place-address'].textContent = address;
-            infowindow.open(map, marker);
         });
-
-        // Sets a listener on a given radio button. The radio buttons specify
-        // the countries used to restrict the autocomplete search.
-        function setupClickListener(id, countries) {
-            var radioButton = document.getElementById(id);
-            radioButton.addEventListener('click', function() {
-                autocomplete.setComponentRestrictions({ 'country': countries });
-            });
-        }
-
-        setupClickListener('changecountry-ph', 'ph');
-        setupClickListener('changecountry-ph-and-uot', ['ph']);
     }
 
     var reInitInterval = setInterval(function() {
