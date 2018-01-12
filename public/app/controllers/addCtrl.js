319e1dc8cbe282cdeeb035ad9551bc3b3b8518b0
Chaelgutierrez <michael@vielsoft.com>
2018-01-12 11:50:16 +0800

    Added auto reinit map

--- public/app/controllers/addCtrl.js
+++ public/app/controllers/addCtrl.js
@@ -1,6 +1,133 @@
 var addCtrl = angular.module('addCtrl', ['geolocation', 'gservice']);
 addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice) {
 
+    function initMap() {
+        var curLat = 13.764;
+        var curLong = 121.055;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                curLat = position.coords.latitude;
+                curLong = position.coords.longitude;
+                console.log(curLat + " " + curLong);
+            }, function(err) {
+                console.log(err);
+            });
+        }
+        jQuery.ajax({
+            url: "http://api.animal-surveillance.com:3000/api/disease/all",
+            method: "GET",
+            headers: {
+                "x-access-token": window.localStorage.getItem('token')
+            },
+            processData: false,
+            success: function(res) {
+                var data = res;
+                var locations = [];
+                var counter = 0;
+
+                data.forEach(function(disease, key) {
+                    var toPush = [];
+                    toPush[0] = "<b>Poultry Name: </b> " + disease.poultryname + "<br/>" +
+                        "<b>Poultry Address: </b>" + disease.poultryaddress + "<br/>" +
+                        "<b>Poultry Owner: </b>" + disease.poultryowner + "<br/>" +
+                        "<b>Disease: </b>" + disease.disease;
+                    toPush[1] = JSON.parse(disease.location[1]);
+                    toPush[2] = JSON.parse(disease.location[0]);
+                    toPush[3] = key + 1;
+                    locations.push(toPush);
+                });
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 10,
+                    center: new google.maps.LatLng(curLat, curLong),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+
+                var infowindow = new google.maps.InfoWindow();
+
+                var marker, i;
+
+                for (i = 0; i < locations.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        }
+                    })(marker, i));
+                }
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        });
+        try {
+            var elem = document.getElementById('map');
+            if (elem) {
+                jQuery('input[name=disease]').on('change', function() {
+                    var ds = jQuery(this).val();
+                    var postdata = { disease: ds };
+                    jQuery.ajax({
+                        url: "http://api.animal-surveillance.com:3000/api/disease/query",
+                        method: "POST",
+                        data: postdata,
+                        headers: {
+                            "x-access-token": window.localStorage.getItem('token')
+                        },
+                        success: function(res) {
+                            var data = res;
+                            var locations = [];
+                            var counter = 0;
+
+                            data.forEach(function(disease, key) {
+                                var toPush = [];
+                                toPush[0] = "<b>Poultry Name: </b> " + disease.poultryname + "<br/>" +
+                                    "<b>Poultry Address: </b>" + disease.poultryaddress + "<br/>" +
+                                    "<b>Poultry Owner: </b>" + disease.poultryowner + "<br/>" +
+                                    "<b>Disease: </b>" + disease.disease;
+                                toPush[1] = JSON.parse(disease.location[1]);
+                                toPush[2] = JSON.parse(disease.location[0]);
+                                toPush[3] = key + 1;
+                                locations.push(toPush);
+                            });
+                            var map = new google.maps.Map(document.getElementById('map'), {
+                                zoom: 10,
+                                center: new google.maps.LatLng(curLat, curLong),
+                                mapTypeId: google.maps.MapTypeId.ROADMAP
+                            });
+
+                            var infowindow = new google.maps.InfoWindow();
+
+                            var marker, i;
+
+                            for (i = 0; i < locations.length; i++) {
+                                marker = new google.maps.Marker({
+                                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                                    map: map
+                                });
+
+                                google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                                    return function() {
+                                        infowindow.setContent(locations[i][0]);
+                                        infowindow.open(map, marker);
+                                    }
+                                })(marker, i));
+                            }
+                        },
+                        error: function(err) {
+                            console.log(err);
+                        }
+                    });
+
+                });
+            }
+        } catch (err) {}
+    }
+    initMap();
+
     $scope.options = {
         legend: {
             display: true,
@@ -55,7 +182,6 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
                 $scope.formData.poultryowner = "";
                 $scope.formData.poultryaddress = "";
                 $scope.formData.disease = "";
-                window.location.reload();
             })
             .error(function(data) {
                 console.log('Error: ' + data);
